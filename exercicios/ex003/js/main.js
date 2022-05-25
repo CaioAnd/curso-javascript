83241d18e51a4c655d4b2a71bdfd9a5b79375a16
Caio_Andrade <caio.mk.andrade@gmail.com>
2022-05-25 15:59:09 -0300

    tabela incompleta

--- exercicios/ex003/js/main.js
+++ exercicios/ex003/js/main.js
@@ -6,16 +6,19 @@ botaoFinalizar.addEventListener("click", function(event){
 
     var entrada = document.getElementById("entrada");
     var opcao1 = entrada.options[entrada.selectedIndex].textContent;
+    var valor1 = parseFloat(entrada.options[entrada.selectedIndex].value);
 
     var principal = document.getElementById("principal");
     var opcao2 = principal.options[principal.selectedIndex].textContent;
-    console.log(opcao2);
+    var valor2 = parseFloat(principal.options[principal.selectedIndex].value);
     
     var bebidas = document.getElementById("bebidas");
     var opcao3 = bebidas.options[bebidas.selectedIndex].textContent;
+    var valor3 = parseFloat(bebidas.options[bebidas.selectedIndex].value);
 
     var sobremesa = document.getElementById("sobremesa");
     var opcao4 = sobremesa.options[sobremesa.selectedIndex].textContent;
+    var valor4 = parseFloat(sobremesa.options[sobremesa.selectedIndex].value);
 
     var pagamento = document.getElementById("pagamento");
     var opcao5 = pagamento.options[pagamento.selectedIndex].textContent;
@@ -26,12 +29,14 @@ botaoFinalizar.addEventListener("click", function(event){
         var thBebidas = document.createElement("th");
         var thSobremesa = document.createElement("th");
         var thPagamento = document.createElement("th");
+        var thTotal = document.createElement("th");
 
         thEntrada.textContent = "Entrada";
         thPrincipal.textContent = "Prato principal";
         thBebidas.textContent = "Bebidas";
         thSobremesa.textContent = "Sobremesa";
         thPagamento.textContent = "Forma de pagamento";
+        thTotal.textContent = "Valor total";
 
         var tabelaCabeca = document.querySelector("#tabela-cabeca");
         var trTabelaCabeca = document.createElement("tr");
@@ -43,21 +48,27 @@ botaoFinalizar.addEventListener("click", function(event){
         trTabelaCabeca.appendChild(thBebidas);
         trTabelaCabeca.appendChild(thSobremesa);
         trTabelaCabeca.appendChild(thPagamento);
+        trTabelaCabeca.appendChild(thTotal);
 
         x++;
     }
+
+    var valorTotal = parseFloat(valor1 + valor2 + valor3 + valor4);
 
     var tdEntrada = document.createElement("td");
     var tdPrincipal = document.createElement("td");
     var tdBebidas = document.createElement("td");
     var tdSobremesa = document.createElement("td");
     var tdPagamento = document.createElement("td");
+    var tdTotal = document.createElement("td");
+
 
     tdEntrada.textContent = opcao1;
     tdPrincipal.textContent = opcao2;
     tdBebidas.textContent = opcao3;
     tdSobremesa.textContent = opcao4;
     tdPagamento.textContent = opcao5;
+    tdTotal.textContent = valorTotal;
 
     var tabelaCorpo = document.querySelector("#tabela-corpo");
     var trTabelaCorpo = document.createElement("tr");
@@ -69,5 +80,13 @@ botaoFinalizar.addEventListener("click", function(event){
     trTabelaCorpo.appendChild(tdBebidas);
     trTabelaCorpo.appendChild(tdSobremesa);
     trTabelaCorpo.appendChild(tdPagamento);
+    trTabelaCorpo.appendChild(tdTotal);
+
+    ajustarTexto(tdEntrada);
     
 });
+
+function ajustarTexto(tdEntrada){
+    const chars = tdEntrada.split('-');
+    /* console.log(chars[1]); */
+}
